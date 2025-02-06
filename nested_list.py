3f5465ace0c91319d9e7fce431e203964ac2ae9c
Alfred Darkwa <darkwaalfred64@gmail.com>
2025-02-06 14:36:12 +0000

    Update nested_list.py

--- nested_list.py
+++ nested_list.py
@@ -9,12 +9,16 @@ if __name__ == '__main__':
     for i in range(len(record)):
         score_lst.append(record[i][1])
     score_lst.sort()
-    s_l_grade = score_lst[1]
+    a = score_lst[0]
+    while a in score_lst:
+        score_lst.remove(a)
+    s_l_grade = score_lst[0]
     name =[]
     for i in range(len(record)):
         if record[i][1] == s_l_grade:
-            name.append(record[i][0])
-        name.sort()
+            name.append(record[i][0]) 
+    name.sort()
     for i in name:
         print(i)
+   
         
